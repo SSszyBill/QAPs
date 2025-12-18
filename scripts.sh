6a61f498f9b52377ff9040e8ffafe985cae15c92
jxxiong <222042015@link.cuhk.edu.cn>
2025-12-18 17:41:06 +0800

    add baseline

--- scripts.sh
+++ scripts.sh
@@ -1,9 +1,17 @@
-seed=42
-iters=2000
-batch_size=1000
+# seed=42
+# iters=2000
+# batch_size=1000
+
+# timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+# echo "==========Running script at $timestamp==========" >> result.txt
+# for instance in $(cat qap_instances.txt); do
+#     python main_gpu.py --instance $instance --seed $seed --iters $iters --batch_size $batch_size
+# done
+
+
 
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-echo "==========Running script at $timestamp==========" >> result.txt
+echo "==========Running script at $timestamp==========" >> result_gurobi.txt
 for instance in $(cat qap_instances.txt); do
-    python main_gpu.py --instance $instance --seed $seed --iters $iters --batch_size $batch_size
+    python test_baseline.py --instance $instance
 done
