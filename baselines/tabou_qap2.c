9fb6a3770595883f7e15517d970094ab0d2afb3a
jxxiong <222042015@link.cuhk.edu.cn>
2025-12-28 22:42:19 +0800

    update

--- baselines/tabou_qap2.c
+++ baselines/tabou_qap2.c
@@ -1,42 +1,32 @@
 /*****************************************************************
  Implementation of the robust taboo search of: E. Taillard
- "Robust taboo search for the quadratic assignment problem", 
- Parallel Computing 17, 1991, 443-455.
-
- Data file format: 
-  n, optimum solution value
- (nxn) flow matrix,
- (nxn) distance matrix
-
- Copyright : E. Taillard, 1990-2004
- Standard C version with slight improvement regarding to
- 1991 version: non uniform tabu duration
-E. Taillard, 14.03.2006
-
- Compatibility: Unix and windows gcc, g++, bcc32.
- This code can be freely used for non-commercial purpose.
- Any use of this implementation or a modification of the code
- must acknowledge the work of E. Taillard
-
+ Modified for:
+ 1. Double/Float matrix support
+ 2. CLI arguments for filename
+ 3. Automatic Timing
+ 4. Result Logging (Instance Name, Time, Obj)
 ****************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>   /* For timing */
+#include <string.h> /* For filename manipulation */
 
-const int infinite = 999999999;
+const double infinite = 1.0e30; 
 const int FALSE = 0;
 const int TRUE = 1;
 
-typedef int*  type_vector;
-typedef int** type_matrix;
+typedef int* type_vector;
+typedef double** type_matrix; 
 
-int opt;
-double  somme_sol = 0.0;
+double opt;
+double somme_sol = 0.0;
 
 /*************** L'Ecuyer random number generator ***************/
 double rando()
  {
-  static int x10 = 12345, x11 = 67890, x12 = 13579, /* initial value*/
-             x20 = 24680, x21 = 98765, x22 = 43210; /* of seeds*/
+  static int x10 = 12345, x11 = 67890, x12 = 13579, 
+             x20 = 24680, x21 = 98765, x22 = 43210; 
   const int m = 2147483647; const int m2 = 2145483479; 
   const int a12= 63308; const int q12=33921; const int r12=12979; 
   const int a13=-183326; const int q13=11714; const int r13=2883; 
@@ -56,24 +46,23 @@ double rando()
   if (h == 0) return(1.0); else return(h*invm);
  }
 
-/*********** return an integer between low and high *************/
 int unif(int low, int high)
  {return low + (int)((double)(high - low + 1) * rando()) ;}
 
 void transpose(int *a, int *b) {int temp = *a; *a = *b; *b = temp;}
 
-int min(int a, int b) {if (a < b) return(a); else return(b);}
+double min_d(double a, double b) {if (a < b) return(a); else return(b);}
 
 double cube(double x) {return x*x*x;}
 
-
 /*--------------------------------------------------------------*/
-/*       compute the cost difference if elements i and j        */
-/*         are transposed in permutation (solution) p           */
+/* compute the cost difference                                  */
 /*--------------------------------------------------------------*/
-int compute_delta(int n, type_matrix a, type_matrix b,
+double compute_delta(int n, type_matrix a, type_matrix b,
                    type_vector p, int i, int j)
- {int d; int k;
+ {
+  double d;
+  int k;
   d = (a[i][i]-a[j][j])*(b[p[j]][p[j]]-b[p[i]][p[i]]) +
       (a[i][j]-a[j][i])*(b[p[j]][p[i]]-b[p[i]][p[j]]);
   for (k = 0; k < n; k = k + 1) if (k!=i && k!=j)
@@ -83,10 +72,9 @@ int compute_delta(int n, type_matrix a, type_matrix b,
  }
 
 /*--------------------------------------------------------------*/
-/*      Idem, but the value of delta[i][j] is supposed to       */
-/*    be known before the transposition of elements r and s     */
+/* Idem, but the value of delta[i][j] is known                  */
 /*--------------------------------------------------------------*/
-int compute_delta_part(type_matrix a, type_matrix b,
+double compute_delta_part(type_matrix a, type_matrix b,
                         type_vector p, type_matrix delta, 
                         int i, int j, int r, int s)
   {return(delta[i][j]+(a[r][i]-a[r][j]+a[s][j]-a[s][i])*
@@ -99,37 +87,35 @@ void tabu_search(int n,                  /* problem size */
                  type_matrix a,          /* flows matrix */
                  type_matrix b,          /* distance matrix */
                  type_vector best_sol,   /* best solution found */
-                 int *best_cost,         /* cost of best solution */
-                 int tabu_duration,      /* parameter 1 (< n^2/2) */
-                 int aspiration,         /* parameter 2 (> n^2/2)*/
-                 int nr_iterations)      /* number of iterations */ 
+                 double *best_cost,      /* cost */
+                 int tabu_duration,      
+                 int aspiration,         
+                 int nr_iterations)       
            
  
  {type_vector p;                        /* current solution */
   type_matrix delta;                    /* store move costs */
-  type_matrix tabu_list;                /* tabu status */
+  int** tabu_list;                      /* tabu status */
   int current_iteration;                /* current iteration */
-  int current_cost;                     /* current sol. value */
+  double current_cost;                  /* current sol. value */
   int i, j, k, i_retained, j_retained;  /* indices */
-  int min_delta;                        /* retained move cost */
+  double min_delta;                     /* retained move cost */
   int autorized;                        /* move not tabu? */
   int aspired;                          /* move forced? */
   int already_aspired;                  /* in case many moves forced */
 
   /***************** dynamic memory allocation *******************/
   p = (int*)calloc(n, sizeof(int));
-  delta = (int**)calloc(n,sizeof(int*));
-  for (i = 0; i < n; i = i+1) delta[i] = (int*)calloc(n, sizeof(int));
+  delta = (double**)calloc(n,sizeof(double*));
+  for (i = 0; i < n; i = i+1) delta[i] = (double*)calloc(n, sizeof(double));
   tabu_list = (int**)calloc(n,sizeof(int*));
   for (i = 0; i < n; i = i+1) tabu_list[i] = (int*)calloc(n, sizeof(int));
-
 
   /************** current solution initialization ****************/
   for (i = 0; i < n; i = i + 1) p[i] = best_sol[i];
 
   /********** initialization of current solution value ***********/
-  /**************** and matrix of cost of moves  *****************/
-  current_cost = 0;
+  current_cost = 0.0;
   for (i = 0; i < n; i = i + 1) for (j = 0; j < n; j = j + 1)
    {current_cost = current_cost + a[i][j] * b[p[i]][p[j]];
     if (i < j) {delta[i][j] = compute_delta(n, a, b, p, i, j);};
@@ -141,11 +127,11 @@ void tabu_search(int n,                  /* problem size */
     tabu_list[i][j] = -(n*i + j);
 
   /******************** main tabu search loop ********************/
-  for (current_iteration = 1; current_iteration <= nr_iterations && *best_cost > opt; 
+  for (current_iteration = 1; current_iteration <= nr_iterations; 
        current_iteration = current_iteration + 1)
-   {/** find best move (i_retained, j_retained) **/
-    i_retained = infinite;       /* in case all moves are tabu */
-    j_retained = infinite;
+   {
+    i_retained = -1;       
+    j_retained = -1;
     min_delta = infinite;
     already_aspired = FALSE;
     
@@ -157,12 +143,12 @@ void tabu_search(int n,                  /* problem size */
         aspired =
          (tabu_list[i][p[j]] < current_iteration-aspiration)||
          (tabu_list[j][p[i]] < current_iteration-aspiration)||
-         (current_cost + delta[i][j] < *best_cost);                
+         (current_cost + delta[i][j] < *best_cost - 0.000001);               
 
-        if ((aspired && !already_aspired) || /* first move aspired*/
-           (aspired && already_aspired &&    /* many move aspired*/
-            (delta[i][j] < min_delta)   ) || /* => take best one*/
-           (!aspired && !already_aspired &&  /* no move aspired yet*/
+        if ((aspired && !already_aspired) || 
+           (aspired && already_aspired &&    
+            (delta[i][j] < min_delta)   ) || 
+           (!aspired && !already_aspired &&  
             (delta[i][j] < min_delta) && autorized))
           {i_retained = i; j_retained = j;
            min_delta = delta[i][j];
@@ -170,26 +156,22 @@ void tabu_search(int n,                  /* problem size */
           };
        };
 
-    if (i_retained == infinite) printf("All moves are tabu! \n"); 
+    if (i_retained == -1) printf("All moves are tabu! \n"); 
     else 
-     {/** transpose elements in pos. i_retained and j_retained **/
+     {
       transpose(&p[i_retained], &p[j_retained]);
-      /* update solution value*/
       current_cost = current_cost + delta[i_retained][j_retained];
-      /* forbid reverse move for a random number of iterations*/
+      
       tabu_list[i_retained][p[j_retained]] = 
         current_iteration + (int)(cube(rando())*tabu_duration);
       tabu_list[j_retained][p[i_retained]] = 
          current_iteration + (int)(cube(rando())*tabu_duration);
 
-      /* best solution improved ?*/
       if (current_cost < *best_cost)
        {*best_cost = current_cost;
         for (k = 0; k < n; k = k+1) best_sol[k] = p[k];
-        printf("Solution of value: %d found at iter. %d\n", current_cost, current_iteration);
        };
 
-      /* update matrix of the move costs*/
       for (i = 0; i < n-1; i = i+1) for (j = i+1; j < n; j = j+1)
         if (i != i_retained && i != j_retained && 
             j != i_retained && j != j_retained)
@@ -201,12 +183,10 @@ void tabu_search(int n,                  /* problem size */
      };
       
    }; 
-  /* free memory*/
   free(p);
   for (i=0; i < n; i = i+1) free(delta[i]); free(delta);
-  for (i=0; i < n; i = i+1) free(tabu_list[i]); 
-  free(tabu_list);
-} /* tabu*/
+  for (i=0; i < n; i = i+1) free(tabu_list[i]); free(tabu_list);
+} 
 
 void generate_random_solution(int n, type_vector  p)
  {int i;
@@ -214,74 +194,139 @@ void generate_random_solution(int n, type_vector  p)
   for (i = 0; i < n-1; i++) transpose(&p[i], &p[unif(i, n-1)]);
  }
 
-int main()
- {int n;                    /* problem size */
-  type_matrix a, b;         /* flows and distances matrices*/
-  type_vector solution;     /* solution (permutation) */
-  int cost;                 /* solution cost */
-  int nr_iterations,        /* number of tabu search iterations */
-      nr_resolutions, no_res; /* number of trials */
+/* Helper to extract instance name */
+void extract_instance_name(const char* filepath, char* output) {
+    const char *last_slash = strrchr(filepath, '/');
+    const char *last_backslash = strrchr(filepath, '\\');
+    const char *filename_start = filepath;
+    char *dot;
 
-  FILE* data_file;       
-  char file_name[30];
+    /* Find start of filename (handle Linux/Windows paths) */
+    if (last_slash != NULL && last_slash > last_backslash) {
+        filename_start = last_slash + 1;
+    } else if (last_backslash != NULL) {
+        filename_start = last_backslash + 1;
+    }
+
+    /* Copy to output buffer */
+    strcpy(output, filename_start);
+
+    /* Remove extension */
+    dot = strrchr(output, '.');
+    if (dot != NULL) {
+        *dot = '\0';
+    }
+}
+
+int main(int argc, char *argv[])
+ {
+  /* --- Configuration --- */
+
+  /* --------------------- */
+
+  int n;                    
+  type_matrix a, b;         
+  type_vector solution;     
+  double cost;              
+  int no_res;
+  
+  double global_best_cost = infinite; 
+  clock_t start_time, end_time;
+  double total_time_elapsed;
+
+  FILE* data_file;
+  FILE* result_file;      
+  char* file_path_arg;
+  char instance_name[256]; /* Buffer for extracted name */
   int i, j;
   char bidon[1000];
 
-  /************** read file name and problem size ***************/
-  printf("Data file name : \n");
-  scanf("%s",file_name); printf(file_name); printf("\n");
-  data_file = fopen(file_name,"r");
-  fscanf(data_file,"%d%d", &n, &opt);
+  if (argc < 2) {
+      printf("Usage: %s <data_file_path>\n", argv[0]);
+      return 1;
+  }
+  file_path_arg = argv[1];
+
+  /* 1. Extract instance name */
+  extract_instance_name(file_path_arg, instance_name);
+
+  printf("Running Robust Tabu Search...\n");
+  printf("Full Path: %s\n", file_path_arg);
+  printf("Instance Name: %s\n", instance_name);
+
+  data_file = fopen(file_path_arg,"r");
+  if (data_file == NULL) {
+      printf("Error: Could not open file %s\n", file_path_arg);
+      return 1;
+  }
+
+  // fscanf(data_file,"%d%lf", &n, &opt);
+  fscanf(data_file,"%d", &n);
   fscanf(data_file,"%[^\n]", bidon);
 
-  printf("Number of tabu search iterations:\n");
-  scanf("%d",&nr_iterations);
-  printf("Number of trials:\n");
-  scanf("%d",&nr_resolutions);
-
-  /****************** dynamic memory allocation ******************/
+  /****************** Memory Allocation ******************/
   solution = (int*)calloc(n, sizeof(int));
+  a = (double**)calloc(n, sizeof(double*));
+  for (i = 0; i < n; i = i+1) a[i] = (double*)calloc(n, sizeof(double));
+  b = (double**)calloc(n,sizeof(double*));
+  for (i = 0; i < n; i = i+1) b[i] = (double*)calloc(n, sizeof(double));
 
-  a = (int**)calloc(n, sizeof(int*));
-  for (i = 0; i < n; i = i+1) a[i] = (int*)calloc(n, sizeof(int));
-  b = (int**)calloc(n,sizeof(int*));
-  for (i = 0; i < n; i = i+1) b[i] = (int*)calloc(n, sizeof(int));
-
-  /************** read flows and distances matrices **************/
   for (i = 0; i < n; i++) for (j = 0; j < n; j = j+1)
-    fscanf(data_file,"%d", &a[i][j]);
-
+    fscanf(data_file,"%lf", &a[i][j]);
 
   for (i = 0; i < n; i = i+1) for (j = 0; j < n; j = j+1)
-    fscanf(data_file,"%d", &b[i][j]);
+    fscanf(data_file,"%lf", &b[i][j]);
     
   fclose(data_file);
+
+  int nr_iterations = 1000 * n;
+  int nr_resolutions = 10;
   
+  /* --- START TIMING --- */
+  start_time = clock();
+
   for(no_res = 1; no_res <= nr_resolutions; no_res++)
-   {generate_random_solution(n, solution);
+   {
+    generate_random_solution(n, solution);
 
-    tabu_search(n, a, b,                     /* problem data */
-               solution, &cost,              /* tabu search results */
-               8*n, n*n*5,                   /* parameters */
-               nr_iterations);               /* number of iterations */
+    tabu_search(n, a, b,                     
+               solution, &cost,              
+               8*n, n*n*5,                   
+               nr_iterations);               
 
-    printf("%d Solution found by tabu search:\n", cost);
-    for (i = 0; i < n; i = i+1) printf("%d ", solution[i]); 
-    printf("\n");
+    if (cost < global_best_cost) {
+        global_best_cost = cost;
+    }
+
+    printf("Trial %d/%d Cost: %f\n", no_res, nr_resolutions, cost);
     somme_sol += cost;
    }
    
-   printf("Average cost: %f, average dev: %f\n", 
-          somme_sol/nr_resolutions, 100*(somme_sol/nr_resolutions - opt)/opt);
+  /* --- END TIMING --- */
+  end_time = clock();
+  total_time_elapsed = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+  printf("\n--- Final Results ---\n");
+  printf("Instance: %s\n", instance_name);
+  printf("Global Best Cost: %f\n", global_best_cost);
+  printf("Total Time: %f seconds\n", total_time_elapsed);
+
+  /* Write result to result.txt (Append Mode) */
+  result_file = fopen("tabou_qap2_result.txt", "a");
+  if (result_file == NULL) {
+      printf("Error opening result.txt for writing.\n");
+  } else {
+      /* Format: instance_name, time, best_obj */
+      fprintf(result_file, "%s %.6f %.6f\n", instance_name, total_time_elapsed, global_best_cost);
+      fclose(result_file);
+      printf("Result saved to result.txt\n");
+  }
 
   free(solution);
   for (i = 0; i < n; i = i+1) free(b[i]);
   free(b);
   for (i = 0; i < n; i = i+1) free(a[i]);
   free(a);
-  fflush(stdin);
-  printf("Press return to end programme\n");
-  getchar();
+
   return EXIT_SUCCESS;
  }
- 
