bf88839f12fdbb8d6f3e3c37d8fe352b12dec2f5
jx-xiong <222042015@link.cuhk.edu.cn>
2026-01-25 17:18:27 +0800

    update

--- baselines/pygm.py
+++ baselines/pygm.py
@@ -73,20 +73,15 @@ if __name__ == "__main__":
     
     try:
         # normalize F and D
-        F = F_np / np.linalg.norm(F_np, ord='fro')
-        D = D_np / np.linalg.norm(D_np, ord='fro')
+        # F = F_np / np.linalg.norm(F_np, ord='fro')
+        # D = D_np / np.linalg.norm(D_np, ord='fro')
         
         start_time = time.time()
+        K = torch.tensor(np.kron(D_np, F_np), dtype=torch.float32)
+        
         n1 = n2 = torch.tensor([n])
-        # Build affinity matrix
-        conn1, edge1, ne1 = pygm.utils.dense_to_sparse(torch.tensor(F, dtype=torch.float32).unsqueeze(0))
-        conn2, edge2, ne2 = pygm.utils.dense_to_sparse(torch.tensor(D, dtype=torch.float32).unsqueeze(0))
-        gaussian_aff = functools.partial(pygm.utils.gaussian_aff_fn, sigma=1) # set affinity function
-        K = pygm.utils.build_aff_mat(None, edge1, conn1, None, edge2, conn2, n1, ne1, n2, ne2, edge_aff_fn=gaussian_aff)
-
-    
-        X = solver(K, n1, n2)
-        X = pygm.hungarian(X)[0]
+        X = solver(-K, n1, n2)
+        X = pygm.hungarian(X)
         
         solve_time = time.time() - start_time
         
